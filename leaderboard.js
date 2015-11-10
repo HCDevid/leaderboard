dc56011f1afbe2181983cebcdcd789b93c66ded4
Don Khan <hcdevid@users.noreply.github.com>
2015-11-09 23:53:30 -0500

    Completed helper functions

--- leaderboard.js
+++ leaderboard.js
@@ -1,7 +1,18 @@
 PlayersList = new Mongo.Collection('players');
   
 if(Meteor.isClient){
-  console.log("hello bros!");
+
+  Template.leaderboard.helpers({
+    'player' : function() {
+      return PlayersList.find()
+    },
+    'playerCount' : function() {
+      return PlayersList.find().count()
+    },
+    'other' : function() {
+      return "other template helper check"
+    }
+  })
 }
 
 if(Meteor.isServer){
